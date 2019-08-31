5785e65c74fc0f950a8b9031411d3cf5b94b4b60
jsun454 <jsun46@umail.ucsb.edu>
2019-08-31 16:49:06 -0700

    Displays 3 rows that can be selected

--- app/src/main/java/com/example/jeffrey/multicasttest2/MainFragment.kt
+++ app/src/main/java/com/example/jeffrey/multicasttest2/MainFragment.kt
@@ -1,15 +1,37 @@
 package com.example.jeffrey.multicasttest2
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
+import androidx.core.content.ContextCompat
 import androidx.leanback.app.BrowseSupportFragment
 import androidx.leanback.widget.ArrayObjectAdapter
+import androidx.leanback.widget.HeaderItem
+import androidx.leanback.widget.ListRow
+import androidx.leanback.widget.ListRowPresenter
 
 class MainFragment : BrowseSupportFragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         Log.d("Jeffrey Sun", "fragment!")
         super.onActivityCreated(savedInstanceState)
-        val adapter = ArrayObjectAdapter(TestPresenter())
+
+        val adapter = ArrayObjectAdapter(ListRowPresenter())
+        for(i in 1..3) {
+            val row = TestRow("Row $i", ArrayObjectAdapter(TestPresenter()))
+            //TestRowView(activity!!.baseContext)
+            val headerItem = HeaderItem(row.name)
+            val listRow = ListRow(headerItem, row.adapter)
+            adapter.add(listRow)
+
+            //Handler(Looper.getMainLooper()).post { adapter.add(TestRowView(activity!!.baseContext)) }
+        }
         setAdapter(adapter)
+
+        //badgeDrawable = ContextCompat.getDrawable(activity!!.baseContext,
+        //    R.drawable.app_icon_your_company)
+        title = "Racers"
+        headersState = BrowseSupportFragment.HEADERS_ENABLED
+        isHeadersTransitionOnBackEnabled = true // ?
     }
 }
---
+++ app/src/main/java/com/example/jeffrey/multicasttest2/TestRow.kt
@@ -0,0 +1,7 @@
+package com.example.jeffrey.multicasttest2
+
+import androidx.leanback.widget.ArrayObjectAdapter
+
+class TestRow(val name: String, val adapter: ArrayObjectAdapter) {
+
+}
